ce45d1f489e2808e74798adfd739ea053ff3afd3
amin <106171699+aminishere@users.noreply.github.com>
2023-05-31 18:02:43 +0530

    Add files via upload

---
+++ in.js
@@ -0,0 +1,7 @@
+function imgslider(any){
+  document.querySelector('.starbucks').src=any;
+}
+function changecolor(color){
+const circle = document.querySelector('.circle');
+circle.style.background = color;
+}
